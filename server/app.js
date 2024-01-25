8b3faa2b76536dae44b1c82a0b78fe9a645c08d9
omar bouassida <57063744+Omar-bit@users.noreply.github.com>
2024-01-25 21:25:18 +0100

    .

--- server/app.js
+++ server/app.js
@@ -19,4 +19,4 @@ app.use('/api/psy', psyRouter);
 app.use('/api/patient', patientRouter);
 app.use('/api/session', sessionRouter);
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Server started on port port'));
+app.listen(port, () => console.log('Server started on port ' + port));
